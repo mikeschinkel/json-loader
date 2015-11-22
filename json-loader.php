ea57933623786a00b4b1bdc6dc733e1765b7af8a
Mike Schinkel <mike@newclarity.net>
2015-11-22 17:08:45 -0500

    Updated @package, @author, $link and @requires (to 5.4) in header.

--- json-loader.php
+++ json-loader.php
@@ -3,13 +3,13 @@
 /**
  * Loads JSON into defined classes to enable default values, data cleansing, validation, etc.
  *
- * @package   JsonLoader
- * @author    Mike Schinkel <mike@newclarity.net>
+ * @package   JSON_Loader
+ * @author    The WPLib Team <http://wplib.org>
  * @copyright 2015 NewClarity Consulting LLC
  * @license   https://en.wikipedia.org/wiki/MIT_License
  * @version   0.1.0
- * @link      http://github.com/mikeschinkel/json-loader
- * @requires  PHP 5.3
+ * @link      http://github.com/wplib/json-loader
+ * @requires  PHP 5.4
  */
 
 require( __DIR__ . '/includes/util.php' );
